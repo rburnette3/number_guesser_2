997b2f5e86da55edeb8a290e687cbed04451c732
Ralph Burnette <rgburnette3@gmail.com>
2017-05-02 13:17:27 -0600

    build on javascript

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-var user
+//global variables//
+
+var userInput = document.querySelector('.user-input');
+
+var guessButton = document.getElementById('guess-button');
+
+var clearButton = document.getElementById('clear-button');
+
+var resetButton = document.getElementById('reset-button');
+
+// var randomNum = Math.floor(Math.random() * (max - min)) + min;
+
+//event listeners//
+
+window.addEventListener('load', function(){
+  generateRandomNumber();
+})
+
+guessButton.addEventListener('click', function(){
+  evaluateGuess()
+})
 
 
+//function//
 
 function generateRandomNumber(min, max){
   var min = 1;
   var max = 100;
-  Math.floor( Math.random() * max - min)) + min;
-
+  var randomNum = Math.floor(Math.random() * (max - min)) + min;
+  console.log(randomNum)
 }
+
+
+// function evaluateGuess()
+//   if (parseInt) {
+//
+//   }
