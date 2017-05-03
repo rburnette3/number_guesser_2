814c27e9088946ea1c46f089368485083d36ea91
Ralph Burnette <rgburnette3@gmail.com>
2017-05-03 10:38:02 -0600

    javascript if and else statement added

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 //global variables//
 
 var userInput = document.querySelector('.user-input');
-
 var guessButton = document.getElementById('guess-button');
-
 var clearButton = document.getElementById('clear-button');
-
 var resetButton = document.getElementById('reset-button');
-
-// var randomNum = Math.floor(Math.random() * (max - min)) + min;
+var userGuessNumber = document.getElementById('user-guess-number')
+var responseGuessText = document.getElementById('response-guess-text')
 
 //event listeners//
 
@@ -20,18 +17,29 @@ guessButton.addEventListener('click', function(){
   evaluateGuess()
 })
 
-
-//function//
+//functions//
 
 function generateRandomNumber(min, max){
   var min = 1;
   var max = 100;
-  var randomNum = Math.floor(Math.random() * (max - min)) + min;
+  randomNum = Math.floor(Math.random() * (100 - 1)) + 1;
   console.log(randomNum)
 }
 
-
-// function evaluateGuess()
-//   if (parseInt) {
-//
-//   }
+function evaluateGuess() {
+  var userGuess = parseInt(userInput.value);
+  console.log(userGuess)
+  userGuessNumber.innerText = userGuess;
+  if (userGuess > 100) {
+    userGuessNumber.innerText = "I Pity the Fool Who Can't Read"
+}  else if (userGuess > randomNum){
+    console.log('your guess is too high');
+    responseGuessText.innerText = "TOO HIGH FOOL"
+} else if (userGuess < randomNum){
+    console.log('your guess is too low');
+    responseGuessText.innerText = "TOO LOW SUCKA"
+  }else {
+    responseGuessText.innerText = "BOOM"
+    console.log('boom')
+}
+}
