16428af4189de71e4cbdd7310030bc993a5987c6
Ralph Burnette <rgburnette3@gmail.com>
2017-05-05 11:58:56 -0600

    add for loop to disable buttons and enable, complete phase 2

--- script.js
+++ script.js
@@ -6,16 +6,47 @@ var clearButton = document.getElementById('clear-button');
 var resetButton = document.getElementById('reset-button');
 var userGuessNumber = document.getElementById('user-guess-number')
 var responseGuessText = document.getElementById('response-guess-text')
-
 //event listeners//
+
+var buttons = document.querySelectorAll('button');
+
+userInput.addEventListener('input' , function() {
+  console.log(this.value);
+
+  // assign min and max
+})
+
+function button() {
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].disabled;
+  }
+}
+
+
 
 window.addEventListener('load', function(){
   generateRandomNumber();
+  enableButton();
 })
 
 guessButton.addEventListener('click', function(){
-  evaluateGuess()
+  evaluateGuess();
 })
+
+userInput.addEventListener('input', function(){
+  enableButton();
+ })
+
+clearButton.addEventListener('click', function(){
+  clearInput();
+})
+
+resetButton.addEventListener('click', function(){
+  generateRandomNumber();
+  reset();
+
+})
+
 
 //functions//
 
@@ -31,15 +62,40 @@ function evaluateGuess() {
   console.log(userGuess)
   userGuessNumber.innerText = userGuess;
   if (userGuess > 100) {
-    userGuessNumber.innerText = "I Pity the Fool Who Can't Read"
+    userGuessNumber.innerText = "ERROR"
 }  else if (userGuess > randomNum){
     console.log('your guess is too high');
-    responseGuessText.innerText = "TOO HIGH FOOL"
+    responseGuessText.innerText = "TOO HIGH"
 } else if (userGuess < randomNum){
     console.log('your guess is too low');
-    responseGuessText.innerText = "TOO LOW SUCKA"
+    responseGuessText.innerText = "TOO LOW"
   }else {
     responseGuessText.innerText = "BOOM"
     console.log('boom')
+  }
 }
-}
+
+function enableButton() {
+  if (userInput.value === ''){
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = true;
+    }
+  } else {
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = false;
+      }
+    }
+  }
+
+  function clearInput() {
+    userInput.value = '';
+    userGuessNumber.innerText = '?';
+  }
+
+  function reset() {
+    userInput.value = '';
+    userGuessNumber.innerText = '?';
+    guessButton.disabled = true;
+    clearButton.disabled = true;
+    resetButton.disabled = true;
+  }
